4e8d5f94001f310bdaf32b8bd8be29a1b21f5104
Sai Bandaru <sai@Sais-MacBook-Pro.local>
2024-10-21 16:10:53 +0530

    handling negative input

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,12 +4,21 @@ class StringCalculator
     # returns 0 if empty string is passed to the method -- add("")
     return 0 if numbers.empty?
 
-    return numbers.to_i if numbers.length == 1
+    delimiter = ','
+    if numbers.start_with?('//')
+      parts = numbers.split("\n", 2)
+      delimiter = parts.first[2..-1]
+      numbers = parts.last
+    end
 
     # Replace '\n' with ','
-    numbers.gsub!("\n", ',')
+    numbers.gsub!("\n", delimiter)
+    num_list = numbers.split(delimiter).map(&:to_i)
 
-    # Handle comma seperated numbers
-    numbers.split(',').map(&:to_i).sum
+    # Raises error when negative numbers are passed
+    negatives = num_list.select { |num| num < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+
+    num_list.sum
   end
 end
