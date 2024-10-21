ed5cc2d10a27566b211cae9758db5aa797b597e7
Sai Bandaru <sai@Sais-MacBook-Pro.local>
2024-10-21 15:00:12 +0530

    add functionality to return single number

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,5 +3,7 @@ class StringCalculator
 
     # returns 0 if empty string is passed to the method -- add("")
     return 0 if numbers.empty?
+
+    return numbers.to_i if numbers.length == 1
   end
 end
